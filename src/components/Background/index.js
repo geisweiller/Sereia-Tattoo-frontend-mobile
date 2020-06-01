59e628c34f3321b74125c834d7c29858b7b260eb
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2020-06-01 11:32:46 -0300

    Initial commit

---
+++ src/components/Background/index.js
@@ -0,0 +1,8 @@
+import LinearGradient from 'react-native-linear-gradient';
+import styled from 'styled-components/native';
+
+export default styled(LinearGradient).attrs({
+  colors: ['#4286f4', '#373b44'],
+})`
+  flex: 1;
+`;
